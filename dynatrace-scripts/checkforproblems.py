9be96cd11f9287e4ba077527008efe2e8c37561d
nikhilgoenkatech <52188007+nikhilgoenkatech@users.noreply.github.com>
2020-09-23 21:45:52 +1000

    Update checkforproblems.py

--- dynatrace-scripts/checkforproblems.py
+++ dynatrace-scripts/checkforproblems.py
@@ -8,7 +8,7 @@ def main():
    endpoint = DT_URL + "api/v1/problem/status"
    get_param = {'Accept':'application/json; charset=utf-8', 'Authorization':'Api-Token {}'.format(DT_TOKEN)}
    config_post = requests.get(endpoint, headers = get_param)
-   jsonObj = applications = json.loads(config_post.text)
+   jsonObj = json.loads(config_post.text)
    problem = jsonObj["result"]["totalOpenProblemsCount"]
    return problem
 
