883ff504552a2c18b301a075961062051f86a264
nikhilgoenkatech <52188007+nikhilgoenkatech@users.noreply.github.com>
2020-10-09 19:35:35 +1100

    Delete cleanup.sh

--- sample-bank-app-service/cleanup.sh
+++
@@ -1,5 +0,0 @@
-IMAGENAME=$1
-docker ps | grep ${IMAGENAME} | awk '{print $1 }' | xargs -I {} docker stop {}
-#docker system prune -a -f 
-#docker rm -f $(docker ps -a -q)
-docker ps -a | awk '{ print $1,$2 }' | grep ${IMAGENAME} | awk '{print $1 }' | xargs -I {} docker rmi {}
